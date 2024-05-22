a80527c02ba655d8f348157f31202df5ecdb275a
shirlei Machado <schiufreitas@gmail.com>
2024-05-22 18:21:45 -0300

    cria DeleteProductService.ts

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,43 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import Product from '../typeorm/entities/Product';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+    if (!product) {
+      throw new AppError('Produto não encontrado');
+    }
+
+    const productsExists = await productsRepository.findByName(name);
+
+    if (productsExists && name !== product.name) {
+      throw new AppError('esse produto já existe');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;

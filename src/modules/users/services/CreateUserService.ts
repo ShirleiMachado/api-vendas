e6b3712c0aea92f459d93ef8b3ba264a552847b0
shirlei Machado <schiufreitas@gmail.com>
2024-06-06 18:51:37 -0300

    cria Services: List e Create

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+    if (emailExists) {
+      throw new AppError('Email já existe');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;

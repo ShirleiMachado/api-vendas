df3d923ce89014d65a9facbad2e20c06cdc80af4
shirlei Machado <schiufreitas@gmail.com>
2024-06-07 10:02:23 -0300

    cria Sessions. Service, controleer e rota

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import userRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
